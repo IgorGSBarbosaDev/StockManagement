c07db345f3e18e189532f472c87343554520260e
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-03-23 21:26:39 -0300

    chore: organize imports in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import app from './app';
+import express from 'express';
+
 const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
+
